38f7269b299cd14b739a88cb1326bb68d1b1a2a0
James Hatfield <james.hatfield17@icloud.com>
2025-08-31 10:02:06 +0100

    Initial project commit. README and Project file created

---
+++ Flashcards.jjhh17/Flashcards.jjhh17/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Flashcards.jjhh17;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
