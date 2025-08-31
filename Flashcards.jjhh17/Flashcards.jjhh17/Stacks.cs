393783c744449875e36fcc25c3e7aa91eb5c86c6
James Hatfield <james.hatfield17@icloud.com>
2025-08-31 10:07:25 +0100

    Created class and constructor for Stack objects

---
+++ Flashcards.jjhh17/Flashcards.jjhh17/Stacks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Stacks
+{
+	public string StackName { get; set; }
+	public string Description { get; set; }
+
+    public Stacks(string name, string description)
+	{
+		StackName = name;
+		Description = description;
+    }
+
+	// For Dapper usage
+	public Stacks() { }
+}
