ce71da36e866a99735a8c9cc05fc187cb2eee81a
James Hatfield <james.hatfield17@icloud.com>
2025-08-31 09:56:13 +0100

    Created class to connect to database, creates stacks table

---
+++ Flashcards.jjhh17/Flashcards.jjhh17/DatabaseConnection.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+using System.Net.NetworkInformation;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.IO;
+using System.Configuration;
+using Microsoft.IdentityModel.Protocols;
+
+namespace Flashcards.jjhh17
+{
+    public class DatabaseConnection
+    {
+
+        private static readonly string server = ConfigurationManager.AppSettings["Server"];
+        private static readonly string instance = ConfigurationManager.AppSettings["DatabaseName"];
+        public static string connectionString = $@"Server=({server})\{instance};Integrated Security=true;";
+
+        public DatabaseConnection()
+        {
+        }
+
+        public static void StackTableCreation()
+        {
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            var sql = @"IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Stacks' and xtype='U')
+                    CREATE TABLE Stacks (
+                        StackName NVARCHAR(50) PRIMARY KEY,
+                        Description NVARCHAR(100),
+                    )";
+            connection.Execute(sql);
+            Console.WriteLine("Table 'Stacks' created.");
+        }
+    }
+}
