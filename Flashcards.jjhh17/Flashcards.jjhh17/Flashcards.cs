03cad6599c4b561ff352a342311fa316504c6e17
James Hatfield <james.hatfield17@icloud.com>
2025-08-31 18:22:37 +0100

    Refactored flashcards class. This now uses a static counter to track how many entries we have

---
+++ Flashcards.jjhh17/Flashcards.jjhh17/Flashcards.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Flashcards.jjhh17
+{
+    public class Flashcards
+    {
+        private static long idCounter = 0;
+
+        private long id { get; set; }
+        public string front { get; set; }
+        public string back { get; set; }
+        public string stackName { get; set; }
+
+        public Flashcards(string front, string back, string stackName)
+        {
+            this.id = ++Flashcards.idCounter;
+            this.front = front;
+            this.back = back;
+            this.stackName = stackName;
+            DatabaseConnection.AddFlashcard(this.id, front, back, stackName);
+        }
+
+        public Flashcards() {}
+
+        public static long GetTotalFlashcardsCreated()
+        {
+            return Flashcards.idCounter;
+        }
+    }
+}
