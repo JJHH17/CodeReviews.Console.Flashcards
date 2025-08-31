7736fc6fc0078ee23857153d073c66a74eae1068
James Hatfield <james.hatfield17@icloud.com>
2025-08-31 16:46:36 +0100

    Created method to print all stack entries

--- Flashcards.jjhh17/Flashcards.jjhh17/UserInterface.cs
+++ Flashcards.jjhh17/Flashcards.jjhh17/UserInterface.cs
@@ -9,6 +9,7 @@ namespace Flashcards.jjhh17
         enum MenuOptions
         {
             CreateStack,
+            PrintAllStacks,
             Exit,
         }
 
@@ -36,6 +37,28 @@ namespace Flashcards.jjhh17
                         string description = Console.ReadLine();
                         Stacks newStack = new Stacks(name, description);
                         AnsiConsole.MarkupLine("[green]Stack created successfully![/]");
+                        Console.WriteLine("Press any key to return to the menu...");
+                        Console.ReadKey();
+                        break;
+
+                    case MenuOptions.PrintAllStacks:
+                        AnsiConsole.MarkupLine("[green]You chose to print all stacks![/]");
+                        var stacks = DatabaseConnection.ReturnAllStacks();
+                        if (stacks.Count == 0)
+                        {
+                            AnsiConsole.MarkupLine("[red]No stacks found.[/]");
+                        }
+                        else
+                        {
+                            var table = new Table();
+                            table.AddColumn("Stack Name");
+                            table.AddColumn("Description");
+                            foreach (var stack in stacks)
+                            {
+                                table.AddRow(stack.StackName, stack.Description);
+                            }
+                            AnsiConsole.Write(table);
+                        }
                         Console.WriteLine("Press any key to return to the menu...");
                         Console.ReadKey();
                         break;
