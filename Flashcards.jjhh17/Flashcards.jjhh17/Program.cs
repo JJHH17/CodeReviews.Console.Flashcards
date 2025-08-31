794c374bb80e72afca93262147a9c4735f367594
James Hatfield <james.hatfield17@icloud.com>
2025-08-31 16:34:07 +0100

    Added a call to the userInterface manu (entry point of the program)

--- Flashcards.jjhh17/Flashcards.jjhh17/Program.cs
+++ Flashcards.jjhh17/Flashcards.jjhh17/Program.cs
@@ -6,9 +6,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Flashcards.jjhh17;
 class Program
 {
-
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        UserInterface.Menu();
     }
 }
