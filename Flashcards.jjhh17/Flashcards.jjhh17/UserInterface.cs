4b75a62955f0217dbfdc7c58456e4aabee418e29
James Hatfield <james.hatfield17@icloud.com>
2025-08-31 16:33:25 +0100

    Created userInterface class. Allows user to add a stack and exit the program loop

---
+++ Flashcards.jjhh17/Flashcards.jjhh17/UserInterface.cs
@@ -0,0 +1,50 @@
+﻿using Spectre.Console;
+using System;
+using Spectre.Console.Cli;
+
+namespace Flashcards.jjhh17
+{
+    public class UserInterface
+    {
+        enum MenuOptions
+        {
+            CreateStack,
+            Exit,
+        }
+
+        public static void Menu()
+        {
+            bool active = true;
+
+            while (active)
+            {
+                Console.Clear();
+                AnsiConsole.MarkupLine("[blue]Welcome to the flashcard application[/]");
+
+                var choice = AnsiConsole.Prompt(
+                    new SelectionPrompt<MenuOptions>()
+                        .Title("Select an option:")
+                        .AddChoices(Enum.GetValues<MenuOptions>()));
+
+                switch (choice)
+                {
+                    case MenuOptions.CreateStack:
+                        AnsiConsole.MarkupLine("[green]You chose to create a new stack![/]");
+                        Console.Write("Enter stack name:");
+                        string name = Console.ReadLine();
+                        Console.Write("Enter stack description:");
+                        string description = Console.ReadLine();
+                        Stacks newStack = new Stacks(name, description);
+                        AnsiConsole.MarkupLine("[green]Stack created successfully![/]");
+                        Console.WriteLine("Press any key to return to the menu...");
+                        Console.ReadKey();
+                        break;
+
+                    case MenuOptions.Exit:
+                        active = false;
+                        break;
+                }
+            }
+        }
+    }
+}
