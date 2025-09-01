a81774e4219b9f5c81a26159460e3d9d37f633f5
James Hatfield <james.hatfield17@icloud.com>
2025-09-01 09:30:32 +0100

    Removed static counter for flashcards class

--- Flashcards.jjhh17/Flashcards.jjhh17/Flashcards.cs
+++ Flashcards.jjhh17/Flashcards.jjhh17/Flashcards.cs
@@ -4,27 +4,18 @@ namespace Flashcards.jjhh17
 {
     public class Flashcards
     {
-        private static long idCounter = 0;
-
-        public long id { get; set; }
         public string front { get; set; }
         public string back { get; set; }
         public string stackName { get; set; }
 
         public Flashcards(string front, string back, string stackName)
         {
-            this.id = ++Flashcards.idCounter;
             this.front = front;
             this.back = back;
             this.stackName = stackName;
-            DatabaseConnection.AddFlashcard(this.id, front, back, stackName);
+            DatabaseConnection.AddFlashcard(front, back, stackName);
         }
 
         public Flashcards() {}
-
-        public static long GetTotalFlashcardsCreated()
-        {
-            return Flashcards.idCounter;
-        }
     }
 }
