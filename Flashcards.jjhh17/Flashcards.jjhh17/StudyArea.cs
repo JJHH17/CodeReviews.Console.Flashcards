bf5ac38deabd1206ad880939aac8629fff6de877
James Hatfield <james.hatfield17@icloud.com>
2025-09-02 10:24:36 +0100

    Created study area class and constructor

---
+++ Flashcards.jjhh17/Flashcards.jjhh17/StudyArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Flashcards.jjhh17
+{
+    public class StudyArea
+    {
+        private string Date { get; set; }
+        private int Score { get; set; }
+        private string StackName { get; set; }
+
+        public StudyArea(string stackName)
+        {
+            Date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            Score = 0;
+            StackName = stackName;
+        }
+
+        public StudyArea() { }
+    }
+}
+
